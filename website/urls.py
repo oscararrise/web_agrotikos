ba055a9663f4f62d7dbb6a0f2927af4d224a0529
oscararrise <oscar.jimenez@arrise.com>
2026-09-24 17:43:26 -0500

    Add dedicated services route

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("servicios/", views.services, name="services"),
     path("robots.txt", views.robots_txt, name="robots"),
     path("sitemap.xml", views.sitemap_xml, name="sitemap"),
 ]
