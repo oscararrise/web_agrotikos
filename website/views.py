7d2cb263826aa760bafa62b1028c42a1573c50c6
oscararrise <oscar.jimenez@arrise.com>
2026-09-24 17:43:28 -0500

    Render services page and add it to sitemap

--- website/views.py
+++ website/views.py
@@ -18,6 +18,10 @@ def home(request):
     return render(request, "website/index.html", {"form": form})
 
 
+def services(request):
+    return render(request, "website/services.html")
+
+
 def robots_txt(request):
     content = "User-agent: *\nAllow: /\nSitemap: https://agrotikos.com/sitemap.xml\n"
     return HttpResponse(content, content_type="text/plain")
@@ -27,5 +31,6 @@ def sitemap_xml(request):
     content = """<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url><loc>https://agrotikos.com/</loc><changefreq>weekly</changefreq><priority>1.0</priority></url>
+  <url><loc>https://agrotikos.com/servicios/</loc><changefreq>weekly</changefreq><priority>0.9</priority></url>
 </urlset>"""
     return HttpResponse(content, content_type="application/xml")
